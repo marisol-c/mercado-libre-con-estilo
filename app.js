1afdc63f5084e8208e0eef7aaab6ab488fb10da0
marisol-c <marisol-contreras-@hotmail.com>
2021-07-05 21:56:58 -0300

    css

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use(express.static('public'));
 
 const path = require("path");
 
-app.listen('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')));
 
 app.listen(port,()=>console.log("servidor corriendo en el puerto 3030"));
